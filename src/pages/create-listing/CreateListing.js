fb500332d5a3018037994ad201d8e95de3b58f8f
Aadhar gaur <aadhar41@gmail.com>
2023-07-01 21:51:35 +0530

    Working on Create Listing page

--- src/pages/create-listing/CreateListing.js
+++ src/pages/create-listing/CreateListing.js
@@ -3,8 +3,19 @@ import './CreateListing.css';
 
 function CreateListing() {
     return (
-        <div>
-            CreateListing
+        <div className='create-listing'>
+            <form className='create-listing-form'>
+                <div className='create-listing-form-group'>
+                    <input type='text' placeholder='Property title' className='create-listing-text' autoFocus={true} />
+                    <label htmlFor='listingImg' className='create-listing-img-upload'>
+                        Upload Image <i class="fa-solid fa-upload"></i>
+                    </label>
+                    <input type='file' className='listingImg' id='listingImg' style={{ display: "none" }} />
+                </div>
+                <div className='create-listing-form-group'>
+                    <textarea className='create-listing-text create-listing-description' placeholder='Property Description'></textarea>
+                </div>
+            </form>
         </div>
     )
 }
