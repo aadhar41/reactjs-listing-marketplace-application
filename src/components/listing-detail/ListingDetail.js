533fe8f2a789871ce455982480441fcb7e027095
Aadhar gaur <aadhar41@gmail.com>
2023-07-01 20:33:17 +0530

    Styling the Listing Detail page

--- src/components/listing-detail/ListingDetail.js
+++ src/components/listing-detail/ListingDetail.js
@@ -4,7 +4,16 @@ import './ListingDetail.css';
 function ListingDetail() {
     return (
         <div className='listing-detail'>
-            ListingDetail component
+            <div className='listing-detail-content'>
+                <img src='https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=3352&h=2286&dpr=1' className='listing-detail' />
+                <h2 className='listing-detail-title'>
+                    Awesome Property
+                    <div className='listing-detail-action'>
+                        <i class="fa-solid fa-pen-to-square"></i>
+                        <i class="fa-solid fa-trash"></i>
+                    </div>
+                </h2>
+            </div>
         </div>
     )
 }
