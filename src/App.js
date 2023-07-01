8c55c9fa89ad83a4e945155b0daa4b67fd45672f
Aadhar gaur <aadhar41@gmail.com>
2023-07-01 23:04:21 +0530

    Adding Navigation and Routing for menu items

--- src/App.js
+++ src/App.js
@@ -4,14 +4,20 @@ import Home from "./pages/homepage/Home";
 import Detail from "./pages/listingdetail/Detail";
 import CreateListing from "./pages/create-listing/CreateListing";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <TopBar />
-      {/* <Home /> */}
-      {/* <Detail /> */}
-      <CreateListing />
-    </div>
+    <Router>
+      <div>
+        <TopBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/detail" element={<Detail />} />
+          <Route exact path="/create" element={<CreateListing />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
